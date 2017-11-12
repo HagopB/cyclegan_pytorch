6fc6121db5bbdcaf1741e9dce473dd84054a5807
HagopB <hagop.boghazdeklian@gmail.com>
2017-11-12 14:02:33 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+import time
+import os
+from options.test_options import TestOptions
+from data.data_loader import CreateDataLoader
+from models.models import create_model
+from util.visualizer import Visualizer
+
+opt = TestOptions().parse()
+opt.nThreads = 1   # test code only supports nThreads = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
+opt.serial_batches = True  # no shuffle
+opt.no_flip = True  # no flip
+
+data_loader = CreateDataLoader(opt)
+dataset = data_loader.load_data()
+model = create_model(opt)
+visualizer = Visualizer(opt)
+# create website
+web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
+
+for i, data in enumerate(dataset):
+    if i >= opt.how_many:
+        break
+    model.set_input(data)
+    model.test()
+    visuals = model.get_current_visuals()
+    img_path = model.get_image_paths()
+    print('process image... %s' % img_path)
+    visualizer.save_preds(visuals, opt.results_dir, i)
